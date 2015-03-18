71661add6f5e32af4dff1de4c732183adcd3ff97
vkp2808 <vkp2808@gmail.com>
2015-03-18 13:14:48 +0530

    commit prg assgment 2

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,49 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  
+  invm <- NULL        # inverse matrix
+  
+  # Set the matrix
+  set <- function(y) {
+    x <<- y
+    invm <<- NULL
+  }
+  # Get the matrix
+  get <- function() x
+  
+  # Set the inverse matrix
+  setinvm <- function(inverse) invm <<- inverse
+  
+  # Get the inverse Matrix
+  getinvm <- function() invm
+  
+  # Return the matrix 
+  list(set = set, get = get, setinvm = setinvm, getinvm = getinvm)
+  
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  invm <- x$getinvm()
+  
+  # If the inverse is already present, return from cache
+  if (!is.null(invm)) {
+    message("getting cached data")
+    return(invm)
+  }
+  
+  # The inverse is not present, so we calculate it
+  data <- x$get()
+  invm <- solve(data, ...)
+  
+  # Cache the inverse
+  x$setinvm(invm)
+  
+  # Return inverse matrix
+  invm
+       
+  
 }
